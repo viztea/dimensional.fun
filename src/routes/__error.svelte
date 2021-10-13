147238b498221527a70cfa7e3207a90635ed154c
melike2d <gino@femboy.email>
2021-10-12 19:23:06 -0700

    :goal_net: error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,24 @@
+<script context="module">
+    export function load({ error, status }) {
+        return {
+            props: {
+                code: status,
+                msg: error.message
+            }
+        };
+    }
+</script>
+
+<script>
+    export let code;
+    export let msg;
+</script>
+
+<div class="flex h-screen px-10">
+    <div class="block text-center items-center m-auto">
+        <div class="flex text-center items-center pb-6">
+            <span class="font-bold text-5xl border-r-1 pr-6 mr-6">{code}</span>
+            <span>{msg}</span>
+        </div>
+    </div>
+</div>
