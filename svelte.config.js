88e8f13f01c2179e277b2b79f4640dd28b620899
Restyled.io <commits@restyled.io>
2021-11-28 03:16:32 +0000

    Restyled by prettier

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,17 @@
 import vercel from '@sveltejs/adapter-vercel';
-import preprocess from "svelte-preprocess";
-import windicss from "vite-plugin-windicss";
+import preprocess from 'svelte-preprocess';
+import windicss from 'vite-plugin-windicss';
 
 /** @type {import("@sveltejs/kit").Config} */
 const config = {
-  preprocess : preprocess(),
-  kit : {
-    target : "#svelte",
-    adapter : vercel(),
-    vite : {
-      plugins : [
-        windicss({config : "windi.config.ts"}),
-      ]
-    },
-  }
+	preprocess: preprocess(),
+	kit: {
+		target: '#svelte',
+		adapter: vercel(),
+		vite: {
+			plugins: [windicss({ config: 'windi.config.ts' })]
+		}
+	}
 };
 
 export default config;
