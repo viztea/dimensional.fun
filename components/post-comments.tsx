1827bb445b08b152e470662762fc7d3bbd4ec92d
melike2d <real.melike2d@gmail.com>
2022-09-21 22:45:13 -0700

    feat: add blog post comments using giscus

---
+++ components/post-comments.tsx
@@ -0,0 +1,19 @@
+import Giscus from "@giscus/react";
+
+export default function() {
+    return (
+        <Giscus
+            repo="melike2d/dimensional.fun"
+            repoId="R_kgDOGNQSUA"
+            category="Blog Posts"
+            categoryId="DIC_kwDOGNQSUM4CRkTl"
+            mapping="title"
+            emitMetadata="0"
+            inputPosition="bottom"
+            theme="transparent_dark"
+            lang="en"
+            loading="lazy"
+            reactionsEnabled="1"
+        />
+    )
+}
