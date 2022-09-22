36922bd13a149cbc2c393088fdec2216b730d5d9
melike2d <real.melike2d@gmail.com>
2022-09-21 20:14:26 -0700

    chore: update my socials

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,14 +1,13 @@
-import { FaDiscord, FaGithub, FaTwitter, FaReddit, FaTwitch, FaTelegramPlane, FaInstagram, FaHome, FaBars, FaBox } from "react-icons/fa";
+import { FaDiscord, FaGithub, FaTwitter, FaReddit, FaTwitch, FaTelegramPlane, FaInstagram, FaHome, FaBars, FaBox, FaLastfm } from "react-icons/fa";
 import { IconType } from "react-icons/lib";
 
 export const socials = [
-    { url: "https://discord.gg/JrHfSmb", Icon: FaDiscord, title: "discord server" },
-    { url: "https://github.com/melike2d", Icon: FaGithub, title: "github profile" },
-    { url: "https://twitter.com/melike2d", Icon: FaTwitter, title: "twitter account" },
-    { url: "https://reddit.com/u/MeLike2D", Icon: FaReddit, title: "reddit profile" },
-    { url: "https://twitch.tv/melike2d", Icon: FaTwitch, title: "twitch channel" },
-    { url: "https://t.me/melike2d", Icon: FaTelegramPlane, title: "telegram account" },
-    { url: "https://instagram.com/melike2d", Icon: FaInstagram, title: "instagram account" }
+    { url: "https://discord.gg/8R4d8RydT4", Icon: FaDiscord, title: "discord server"  },
+    { url: "https://github.com/melike2d",   Icon: FaGithub,  title: "github profile"  },
+    { url: "https://twitter.com/melike2d",  Icon: FaTwitter, title: "twitter profile" },
+    { url: "https://reddit.com/u/melike2d", Icon: FaReddit,  title: "reddit profile"  },
+    { url: "https://twitch.tv/melike2d",    Icon: FaTwitch,  title: "twitch channel"  },
+    { url: "https://last.fm/user/melike2d", Icon: FaLastfm,  title: "lastfm profile"  },
 ]
 
 export type Route = { href: string; name: string; Icon?: IconType }
