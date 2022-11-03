02f896838b97564c22d18a284aad2915454c3b7e
melike2d <real.melike2d@gmail.com>
2022-11-02 18:26:57 -0700

    chore: update alert css

--- components/alert.tsx
+++ components/alert.tsx
@@ -3,16 +3,17 @@ import { PropsWithChildren, useState } from 'react';
 import { FaTimes } from 'react-icons/fa';
 import Container from './container';
 
-type AlertProps = PropsWithChildren<{}>;
+type AlertProps = PropsWithChildren<{ className?: string }>;
 
-export default function Alert({ children }: AlertProps) {
+export default function Alert({ children, className }: AlertProps) {
 	const [closed, setClosed] = useState(false);
 
 	return (
 		<div
 			className={concat(
-				'absolute select-none bottom-0 inset-x-0 bg-black bg-opacity-10 backdrop-blur-3xl border-t border-t-gray-800 py-2.5 justify-center',
-				closed ? 'hidden' : 'flex'
+				'select-none top-0 inset-x-0 bg-black bg-opacity-10 backdrop-blur-3xl border-b border-b-gray-800 py-2.5 justify-center',
+				closed ? 'hidden' : 'flex',
+				className
 			)}
 		>
 			<Container className="flex justify-between">
