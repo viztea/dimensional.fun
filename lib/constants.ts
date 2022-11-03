dd8b0dc838f9c5d1fcc95fc81f861a0029c0dedd
Gino <real.melike2d@gmail.com>
2022-11-02 18:12:15 -0700

    chore: add youtube

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,12 +1,13 @@
-import { FaDiscord, FaGithub, FaTwitter, FaReddit, FaTwitch, FaTelegramPlane, FaInstagram, FaHome, FaBars, FaBox, FaLastfm } from "react-icons/fa";
+import { FaDiscord, FaGithub, FaTwitter, FaReddit, FaTwitch, FaTelegramPlane, FaInstagram, FaHome, FaBars, FaBox, FaYoutube, FaLastfm } from "react-icons/fa";
 import { IconType } from "react-icons/lib";
 
 export const socials = [
     { url: "https://discord.gg/8R4d8RydT4", Icon: FaDiscord, title: "discord server"  },
+    { url: "https://youtube.com/@melike2d", Icon: FaYoutube, title: "youtube channel" }, 
+    { url: "https://twitch.tv/melike2d",    Icon: FaTwitch,  title: "twitch channel"  },
     { url: "https://github.com/melike2d",   Icon: FaGithub,  title: "github profile"  },
     { url: "https://twitter.com/melike2d",  Icon: FaTwitter, title: "twitter profile" },
     { url: "https://reddit.com/u/melike2d", Icon: FaReddit,  title: "reddit profile"  },
-    { url: "https://twitch.tv/melike2d",    Icon: FaTwitch,  title: "twitch channel"  },
     { url: "https://last.fm/user/melike2d", Icon: FaLastfm,  title: "lastfm profile"  },
 ]
 
