c93953354d720b813457ffe1e5a902e77b4e0b96
Gino <real.melike2d@gmail.com>
2022-11-18 18:28:37 -0800

    chore: add umami analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
 	return (
 		<Html lang="en">
-			<Head />
+			<Head>
+				<script async defer data-website-id="747e9bb3-287d-4f18-926b-a1e4350c267b" src="https://analytics.dimensional.fun/umami.js"></script>
+			</Head>
 			<body>
 				<Main />
 				<NextScript />
